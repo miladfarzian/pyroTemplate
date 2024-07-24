c3b7844a19b2b258074156f0746e0e91158ace43
Milad farzian <84211811+miladfarzian@users.noreply.github.com>
2024-07-24 14:41:14 +0330

    Update bot_filters.py

--- bot_filters.py
+++ bot_filters.py
@@ -1,50 +1,34 @@
 from pyrogram import filters
-from configs.config import admins , force_join_channel_id , sudo
-
+from configs.config import admins , force_join_channel_id
+from pyrogram.errors import UserNotParticipant
 
 #--------------{ admin filter }---------------- 
 async def check_if_user_is_admin(_,__,m):
-    try:
-        user_id=m.from_user.id
-        if user_id in admins:
-            return True
-    except:
-        return False    
+    return m.from_user.id in admins
         
 
 
 
 #--------------{ user is memeber filter / forced join }---------------- 
 
-async def check_id_user_joined_channel(_,client,message):
-    user_id = message.from_user.id
+async def check_user_not_in_chat(_,client: Client, message: Message):
     try:
-        await client.get_chat_member(force_join_channel_id, user_id)
-        if int(user_id) not in admins:
-            return  True
-    except:
+        user_id = message.from_user.id
+        member = await client.get_chat_member(force_join_channel_id, user_id)
         return False
+    except UserNotParticipant:
+        print("user is not a memeber of the chat !")
+        return True
 
   
-
-async def check_if_user_is_not_member(_,client,message):
-    user_id = message.from_user.id
+async def check_user_in_chat(_,client: Client, message: Message):
     try:
-        await client.get_chat_member(force_join_channel_id, user_id)
-        
-        return  False 
-    except:
-        if int(user_id) not in admins:
-            return  True
+        user_id = message.from_user.id
+        member = await client.get_chat_member(force_join_channel_id, user_id)
         return True
+    except UserNotParticipant:
+        return False
 
-
-#--------------{ sudo filter }---------------- 
-    
-async def check_if_user_is_sudo(_,client,message):
-    user_id = message.from_user.id
-    if user_id==sudo:
-        return True
 
 
 
@@ -52,6 +36,6 @@ async def check_if_user_is_sudo(_,client,message):
 #list filters 
     
 is_admin = filters.create(func=check_if_user_is_admin)    
-is_member = filters.create(func=check_id_user_joined_channel)    
-not_member = filters.create(func=check_if_user_is_not_member)    
-is_sudo = filters.create(func=check_if_user_is_sudo)    
+is_member = filters.create(func=check_user_in_chat)    
+not_member = filters.create(func=check_user_not_in_chat)    
+
